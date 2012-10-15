d811b24d1639a823db511d0e3443e1b1b9050e3b
sd345din89 <kra.testandroid@gmail.com>
2012-10-15 18:16:47 +0530

    Add Positive Button

--- Test Android Repo/src/com/android/TestAndroidRepoActivity.java
+++ Test Android Repo/src/com/android/TestAndroidRepoActivity.java
@@ -3,7 +3,9 @@ package com.android;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.AlertDialog.Builder;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -21,8 +23,13 @@ public class TestAndroidRepoActivity extends Activity {
 				AlertDialog.Builder builder=new Builder(TestAndroidRepoActivity.this);
 				builder.setTitle("Alert");
 				builder.setMessage("This sample Git Hub Project");
-				builder.setNegativeButton("Ok", null);
-				
+				builder.setNegativeButton("Cancel", null);
+				builder.setPositiveButton("ok", new DialogInterface.OnClickListener() {
+					
+					public void onClick(DialogInterface dialog, int which) {
+						Toast.makeText(TestAndroidRepoActivity.this, "Ok Clicked", Toast.LENGTH_SHORT).show();
+					}
+				});
 				AlertDialog dialog=builder.create();
 				dialog.show();
 				
